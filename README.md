d2901051344b6458ce3f6efc4a75b1d6c669708a
sebv <seb.vincent@gmail.com>
2015-05-21 11:32:30 -0700

    configures travis

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ appium-express
 
 Work in progress, stay tuned!
 
+## Status
+
+[![Build Status](https://travis-ci.org/appium/appium-express.svg)](https://travis-ci.org/appium/appium-express)
+
 ## Watch
 
 ```
--- gulpfile.js
+++ gulpfile.js
@@ -3,4 +3,8 @@
 var gulp = require('gulp'),
     boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
-boilerplate({build: 'appium-express', jscs: false});
+boilerplate({
+  build: 'appium-express',
+  jscs: false,
+  testReporter: process.env.TRAVIS ? 'spec' : 'nyan'
+});
