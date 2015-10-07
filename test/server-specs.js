270858cdc8ef4bc4f149eb81c99fac049e825c0f
Isaac Murchie <isaac@saucelabs.com>
2015-10-07 15:16:32 -0700

    Clean up dependencies and style

--- test/server-specs.js
+++ test/server-specs.js
@@ -1,6 +1,6 @@
 // transpile:mocha
 
-import { server } from '../..';
+import server from '../..';
 import { configureServer } from '../lib/server';
 import request from 'request-promise';
 import chai from 'chai';
@@ -8,7 +8,7 @@ import chaiAsPromised from 'chai-as-promised';
 import sinon from 'sinon';
 
 
-const should = chai.should();
+chai.should();
 chai.use(chaiAsPromised);
 
 describe('server configuration', () => {
@@ -22,61 +22,58 @@ describe('server configuration', () => {
 
   it('should reject if error thrown in configureRoutes parameter', async () => {
     let configureRoutes = () => {
-      throw new Error("I'm Mr. MeeSeeks look at me!");
+      throw new Error('I am Mr. MeeSeeks look at me!');
     };
-    await server(configureRoutes, 8181).should.eventually.be
-           .rejectedWith("MeeSeeks");
+    await server(configureRoutes, 8181).should.be.rejectedWith('MeeSeeks');
   });
 });
 describe('server', () => {
   let hwServer;
+  let errorStub;
   before(async () => {
+    errorStub = sinon.stub(console, 'error');
     function configureRoutes (app) {
       app.get('/', (req, res) => {
         res.header['content-type'] = 'text/html';
-        res.status(200).send("Hello World!");
+        res.status(200).send('Hello World!');
       });
-      app.get('/wd/hub/pythonsucks', (req, res) => {
+      app.get('/wd/hub/python', (req, res) => {
         res.status(200).send(req.headers['content-type']);
       });
       app.get('/error', () => {
-        throw new Error("hahaha");
+        throw new Error('hahaha');
       });
     }
     hwServer = await server(configureRoutes, 8181);
   });
-  it('should start up with our middleware', async () => {
-    let body = await request("http://localhost:8181/");
-    body.should.eql("Hello World!");
-  });
-  it('should start up with our middleware', async () => {
-    let body = await request({
-      url: "http://localhost:8181/wd/hub/pythonsucks",
-      headers: {
-        "user-agent": "Python",
-        "content-type": "application/x-www-form-urlencoded"
-      }
-    });
-    body.should.eql("application/json");
-  });
-  it('should catch errors in the catchall', async () => {
-    let err;
-    try {
-      await request("http://localhost:8181/error");
-    } catch (e) {
-      err = e;
-    }
-    should.exist(err);
-    err.message.should.contain("hahaha");
-  });
-  it('should error if we try to start again on a port that is used', async () => {
-    await server(() => {}, 8181).should.eventually.be.rejectedWith(/EADDRINUSE/);
-  });
-  it('should error if we try to start on a bad hostname', async () => {
-    await server(() => {}, 8181, 'lolcathost').should.eventually.be.rejectedWith(/ENOTFOUND/);
-    await server(() => {}, 8181, '1.1.1.1').should.eventually.be.rejectedWith(/EADDRNOTAVAIL/);
-  });
   after(async () => {
     hwServer.close();
+    errorStub.restore();
+  });
+
+  it('should start up with our middleware', async () => {
+    let body = await request('http://localhost:8181/');
+    body.should.eql('Hello World!');
+  });
+  it('should fix broken context type', async () => {
+    let body = await request({
+      url: 'http://localhost:8181/wd/hub/python',
+      headers: {
+        'user-agent': 'Python',
+        'content-type': 'application/x-www-form-urlencoded'
+      }
+    });
+    body.should.eql('application/json');
+  });
+  it('should catch errors in the catchall', async () => {
+    await request('http://localhost:8181/error')
+      .should.be.rejectedWith(/hahaha/);
+  });
+  it('should error if we try to start again on a port that is used', async () => {
+    await server(() => {}, 8181).should.be.rejectedWith(/EADDRINUSE/);
+  });
+  it('should error if we try to start on a bad hostname', async () => {
+    await server(() => {}, 8181, 'lolcathost').should.be.rejectedWith(/ENOTFOUND/);
+    await server(() => {}, 8181, '1.1.1.1').should.be.rejectedWith(/EADDRNOTAVAIL/);
   });
 });
